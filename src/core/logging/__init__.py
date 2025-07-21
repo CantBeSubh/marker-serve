547275ce2df486044d909c21e6ad7f9d892b70a2
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-07-21 12:23:13 +0530

    feat: Chalk wrapper for logging

---
+++ src/core/logging/__init__.py
@@ -0,0 +1,19 @@
+class Chalk:
+    def __init__(self):
+        from colorama import Fore, Style, init
+
+        init()
+        self.fore = Fore
+        self.style = Style
+
+    def red(self, text):
+        return f"[!] {self.fore.RED}{text}{self.style.RESET_ALL}"
+
+    def green(self, text):
+        return f"[+] {self.fore.GREEN}{text}{self.style.RESET_ALL}"
+
+    def yellow(self, text):
+        return f"[x] {self.fore.YELLOW}{text}{self.style.RESET_ALL}"
+
+    def blue(self, text):
+        return f"[.] {self.fore.BLUE}{text}{self.style.RESET_ALL}"
