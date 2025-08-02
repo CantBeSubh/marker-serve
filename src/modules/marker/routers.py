9e86400dd85d41edf17833a4ca018bb7ace2f212
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-08-02 15:53:51 +0530

    feat: resolve config

--- src/modules/marker/routers.py
+++ src/modules/marker/routers.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, UploadFile,Request
 from typing_extensions import Annotated
 
 from src.core.logging.chalk import Chalk
@@ -26,12 +26,18 @@ def get_models() -> any:
 
 @router.post("/convert", response_model=ConvertResponse)
 async def convert(
+    request:Request,
     file: UploadFile,
     models: Annotated[any, "List of models to use for parsing"] = Depends(get_models),
 ) -> ConvertResponse:
     try:
+        form_data = await request.form()
+        config = dict(form_data)
         chalk.info(f"Converting PDF: {file.filename}")
-        result = await parse_pdf(file, models)
+        del config["file"]
+        chalk.info(f"Config Params: {config.keys()}")
+        chalk.info(f"Config Values: {config}")
+        result = await parse_pdf(file, models, config=config)
         chalk.success(f"PDF converted: {file.filename}")
         return ConvertResponse(**result)
     except Exception as e:
